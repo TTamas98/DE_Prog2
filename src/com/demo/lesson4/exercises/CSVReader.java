34f1073137cc0992c9068f37657cb5e8e0e999c6
Tamas Tarcsa <tamas_tarcsa@epam.com>
2022-10-03 11:26:18 +0200

    Lesson 4 codes added.

---
+++ src/com/demo/lesson4/exercises/CSVReader.java
@@ -0,0 +1,47 @@
+package com.demo.lesson4.exercises;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CSVReader {
+
+    public void printPersons() throws FileNotFoundException {
+        List<Person> personList = readPersonsFromCSV();
+
+        for(Person person: personList) {
+            System.out.println(person);
+        }
+    }
+
+    private List<Person> readPersonsFromCSV() throws FileNotFoundException {
+        List<Person> result = new ArrayList<>();
+        File file = new File("C:\\DE\\Code\\Lessons\\src\\com\\demo\\lesson4\\exercises\\biostats.csv");
+        Scanner scanner = new Scanner(file);
+
+        scanner.useDelimiter("\n");
+        if(scanner.hasNext()){
+            scanner.nextLine();
+        }
+
+        while(scanner.hasNext()) {
+            result.add(convertToPerson(scanner.nextLine()));
+        }
+
+        return result;
+    }
+
+
+    private Person convertToPerson(String line) {
+        String[] fields = line.split(",\\s+");
+        Person person = new Person();
+        person.setName(fields[0].substring(1,fields[0].length()-1));
+        person.setSex(fields[1].substring(1,fields[1].length()-1));
+        person.setAge(Integer.parseInt(fields[2]));
+        person.setHeight(Integer.parseInt(fields[3]));
+        person.setWeight(Integer.parseInt(fields[4]));
+        return person;
+    }
+}
---
+++ src/com/demo/lesson4/exercises/Person.java
@@ -0,0 +1,72 @@
+package com.demo.lesson4.exercises;
+
+public class Person {
+
+    private String name;
+    private String sex;
+    private int age;
+    private int height;
+    private int weight;
+
+    public Person() {
+    }
+
+    public Person(String name, String sex, int age, int height, int weight) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", weight=" + weight +
+                '}';
+    }
+}
